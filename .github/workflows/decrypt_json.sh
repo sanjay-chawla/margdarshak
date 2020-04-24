5cb46db12ba0159d26cc505e7b45610c47a062cd
sanjay-chawla <sans.chawla17@gmail.com>
2020-04-24 22:06:01 +0100

    Add new decryption script

--- .github/workflows/decrypt_json.sh
+++ .github/workflows/decrypt_json.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-cd $HOME/app
-
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPT_PP" \
---output google-services.json google-services.json.gpg
+--output $HOME/app/google-services.json $HOME/app/google-services.json.gpg
 
-cd ..
