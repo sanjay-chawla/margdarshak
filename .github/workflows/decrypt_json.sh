c2a56d5c1c24a9c39b44e467fb4f65c88fc4b05f
sanjay-chawla <sans.chawla17@gmail.com>
2020-04-24 22:23:34 +0100

    debug directory

--- .github/workflows/decrypt_json.sh
+++ .github/workflows/decrypt_json.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+pwd
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPT_PP" \
 --output $HOME/app/google-services.json $HOME/app/google-services.json.gpg
